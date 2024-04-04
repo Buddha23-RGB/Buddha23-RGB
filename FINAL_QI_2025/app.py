561e81782f92ce51e80cbf51b81f16d4fbd51b48
Joe Chamoun <joechamoun23@gmail.com>
2024-04-04 05:28:04 -0600

    Your commit message April4

--- FINAL_QI_2025/app.py
+++ FINAL_QI_2025/app.py
@@ -4,40 +4,44 @@
 # !pip install Flask-Login
 # #%%
 # %pip install flask-login
-from commons import *
+# Import standard libraries
+from flask import Flask
+import os
+import json
+import sqlite3
+from datetime import datetime
+
+# Import third-party libraries
+import pandas as pd
 import yfinance as yf
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.style
 import plotly.graph_objects as go
 import plotly.io as plt_io
-import matplotlib.style
-import matplotlib.pyplot as plt
-import seaborn as sns
-import json
-import pandas as pd
-from datetime import datetime
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from werkzeug.security import generate_password_hash
 from sqlalchemy.orm import Session, relationship
 from sqlalchemy import create_engine, text, inspect
-from werkzeug.security import generate_password_hash
-from flask_login import LoginManager
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, render_template, request, url_for
-from dotenv import load_dotenv
-import os
-import flask_login
-# !pip install schedule
-# !pip install matplotlib
+
+# Import local modules
+from commons import *
+
 # Load environment variables from .env file if it exists
 load_dotenv(
     "C:\\Users\\joech\\OneDrive\\Documents\\Buddha23-RGB\\FINAL_QI_2025\\.github\\.env")
+
+# Set up plotting styles
 sns.set_style('whitegrid')
 pd.core.common.is_list_like = pd.api.types.is_list_like
-
 matplotlib.style.use('dark_background')
 plt.style.use('dark_background')
 plt_io.templates["custom_dark"] = plt_io.templates["plotly_dark"]
-
 plt_io.templates["custom_dark"]['layout']['paper_bgcolor'] = '#30404D'
 plt_io.templates["custom_dark"]['layout']['plot_bgcolor'] = '#30404D'
-
 plt_io.templates['custom_dark']['layout']['yaxis']['gridcolor'] = '#4f687d'
 plt_io.templates['custom_dark']['layout']['xaxis']['gridcolor'] = '#4f687d'
 
@@ -45,6 +49,7 @@ plt_io.templates['custom_dark']['layout']['xaxis']['gridcolor'] = '#4f687d'
 with open('config.json') as f:
     config = json.load(f)
 
+# Set up global variables
 root_dir = config['root_dir']
 rel_path = config['rel_path']
 table_path = config['table_path']
@@ -53,8 +58,7 @@ short_list = config['short_list']
 windows = config['windows']
 final_path = config['final_path']
 
-
-
+# Initialize SQLAlchemy
 db = SQLAlchemy()
 
 
@@ -69,9 +73,24 @@ def create_app():
     return app
 
 
+def create_connection(db_file):
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+        return None
+
+
+def select_all_tasks(conn):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM tasks")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+
 app = create_app()
-
-
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
@@ -105,38 +124,6 @@ def about():
 # @app.route('/chart')
 # def chart():
 #     return render_template('chart.html')
-
-
-def render_table(symbol):
-    with open(f"templates/tables/{symbol}.html") as table:
-        template = Template(table.read())
-        # Load data for the table
-        data = pd.read_csv(f'data/{symbol}.csv')
-        return template.render(
-            data=data.to_dict(orient='records')
-        )
-
-
-@app.route('/tables/<symbol>.html')
-def table(symbol):
-    return render_table(symbol)
-
-
-@app.route('/data', methods=['POST', 'GET'])
-def data():
-    form_data = None
-    if request.method == 'POST':
-        form_data = request.form
-        symbol = form_data.get('symbol')
-        if symbol and isinstance(symbol, str):
-            if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-                # AJAX request, return only the table HTML
-                return render_table(symbol)
-            else:
-                # Normal request, return the full page with the image
-                image_url = url_for('static', filename=f'images/{symbol}.png')
-                return render_template('/data.html', form_data=form_data, image_url=image_url)
-        return render_template('index.html', form_data=form_data)
 
 
 
@@ -214,3 +201,35 @@ if __name__ == '__main__':
 #%%
 
 #%%
+
+
+def render_table(symbol):
+    with open(f"templates/tables/{symbol}.html") as table:
+        template = Template(table.read())
+        # Load data for the table
+        data = pd.read_csv(f'data/{symbol}.csv')
+        return template.render(
+            data=data.to_dict(orient='records')
+        )
+
+
+@app.route('/tables/<symbol>.html')
+def table(symbol):
+    return render_table(symbol)
+
+
+@app.route('/data', methods=['POST', 'GET'])
+def data():
+    form_data = None
+    if request.method == 'POST':
+        form_data = request.form
+        symbol = form_data.get('symbol')
+        if symbol and isinstance(symbol, str):
+            if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+                # AJAX request, return only the table HTML
+                return render_table(symbol)
+            else:
+                # Normal request, return the full page with the image
+                image_url = url_for('static', filename=f'images/{symbol}.png')
+                return render_template('/data.html', form_data=form_data, image_url=image_url)
+        return render_template('index.html', form_data=form_data)
